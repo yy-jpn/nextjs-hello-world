05ebf9966cce74607abcbbbcdb5a5ef0e927d170
yy-jpn <y.yoneda2511@gmail.com>
2026-02-13 10:07:47 +0900

    Initial commit: Hello World Next.js app

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontSize: '2rem' }}>
+      <h1>Hello World</h1>
+    </main>
+  );
+}
